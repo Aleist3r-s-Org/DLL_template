bf5021b033bcac85c654f171983091e6286c7352
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2025-05-14 17:30:02 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "main.hpp"
 
-int CustomExtensions(uint64_t* ptrArray)
+#include <iostream>
+
+// ptrArray is storing pointers to various global variables
+// for example ptrArray[0] is a pointer to std::vector<std::string> g_cmdLineArgs;
+// documentation will exist in the future when all pointers are done
+int CustomExtensions(uint64_t *ptrArray)
 {
+    std::cout << "Hello from CustomExtensions.dll\n";
     return 0;
 }
